9039499f4145828c3981b7580e4b5ed4cd335ece
Adam Debski <adamdebski33@gmail.com>
2017-10-24 17:44:57 +0200

    Pierwszy commit

---
+++ src/main/java/com/debski/ThymeleafTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.debski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafTutorialApplication.class, args);
+	}
+}
